ba17af0af5f845c4fe5c19dd443afe81b9253b89
Vohumana <david.linden.james@gmail.com>
2016-08-17 23:26:06 -0700

    Nuking this in favor of using Hugo

--- deploy.cmd
+++
@@ -1,7 +0,0 @@
-call docpad generate -e static
-xcopy out C:\Sandbox\projects\azuredeployment\ /E
-cd C:\Sandbox\projects\azuredeployment
-git add *
-git commit -m "Deployment"
-git push azure master
-cd C:\Sandbox\projects\github.com\vohumana\vohumana-blog
